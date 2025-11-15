a98bb02bfc110c1c0abda8b88751b230f267dfe8
Ritchie-rr <rregistr@hamilton.edu>
2025-11-15 13:20:31 -0500

    testing the vid1

--- src/test.py
+++ src/test.py
@@ -1,11 +1,12 @@
-import cv2
 import pyvirtualcam
+import cv2
 
 cap = cv2.VideoCapture(0)
-with pyvirtualcam.Camera(width=640, height=480, fps=20) as cam:
+
+with pyvirtualcam.Camera(width=640, height=480, fps=20, backend='obs') as cam:
     print(f'Using virtual camera: {cam.device}')
     while True:
         ret, frame = cap.read()
-        frame = cv2.flip(frame,1)
+        frame = cv2.flip(frame, 1)
         cam.send(frame)
         cam.sleep_until_next_frame()

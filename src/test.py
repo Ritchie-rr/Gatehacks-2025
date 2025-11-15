bde12156a302bb607fd348e48035be0b10b5cb25
Ritchie-rr <rregistr@hamilton.edu>
2025-11-15 13:18:34 -0500

    testing the vid

--- src/test.py
+++ src/test.py
@@ -1,16 +1,11 @@
-import colorsys
-import numpy as np
+import cv2
 import pyvirtualcam
 
-
-with pyvirtualcam.Camera(width=1280, height=720, fps=20) as cam:
-   print(f'Using virtual camera: {cam.device}')
-   frame = np.zeros((cam.height, cam.width, 3), np.uint8)  # RGB
-   while True:
-       h, s, v = (cam.frames_sent % 100) / 100, 1.0, 1.0
-       r, g, b = colorsys.hsv_to_rgb(h, s, v)
-       frame[:] = (r * 255, g * 255, b * 255)
-       cam.send(frame)
-       cam.sleep_until_next_frame()
-
-
+cap = cv2.VideoCapture(0)
+with pyvirtualcam.Camera(width=640, height=480, fps=20) as cam:
+    print(f'Using virtual camera: {cam.device}')
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.flip(frame,1)
+        cam.send(frame)
+        cam.sleep_until_next_frame()
